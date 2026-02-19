c9cdb9107d430b867b4de62d8a1c008d4b2b7166
romnn <roman.dahm@gmx.de>
2026-02-19 01:59:51 +0100

    ci: fix goreleaser build

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -x
 
 echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | tee /etc/apt/sources.list.d/goreleaser.list
-apt update && apt upgrade
+apt update
 apt install -y git goreleaser mingw-w64
 
 # github actions requires to mark the current git repository as safe
